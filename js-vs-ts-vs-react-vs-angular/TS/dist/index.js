a2f5fd2c451b414fefcd6e9c13967fa5a7d115c9
Work Goonja <workgoonja@goonjas-MacBook-Pro.local>
2024-06-07 13:37:36 -0400

    Typescript initialized

---
+++ js-vs-ts-vs-react-vs-angular/TS/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const greet = (name) => {
+    return `Hello, ${name}!`;
+};
+console.log(greet('I wish she would talk to me'));
---
+++ js-vs-ts-vs-react-vs-angular/TS/src/index.ts
@@ -0,0 +1,5 @@
+const greet = (name: string): string => {
+    return `Hello, ${name}!`;
+  }
+  
+  console.log(greet('I wish she would talk to me'));
